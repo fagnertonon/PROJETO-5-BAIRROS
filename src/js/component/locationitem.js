73db1fee3ec6e55c52649329a4b07c7f1e2b9412
fagner tonon <fagnertonon@hotmail.com>
2018-11-27 16:56:52 -0200

    ajuste localtionItem

--- src/js/component/locationitem.js
+++ src/js/component/locationitem.js
@@ -1,34 +1,18 @@
 import React from 'react';
 
-// export default class LocationItem extends React.Component {
-//     render() {
-//         return (
-//             <li role="button"
-//                 className="place"
-//                 tabIndex="0"
-//                 onKeyPress={this.props.openInfoWindow.bind(this, this.props.data.marker)}
-//                 onClick={this.props.openInfoWindow.bind(this, this.props.data.marker)}>
-//                 <p
-//                     className='place-link'>
-//                     {this.props.data.name}
-//                 </p>
-//             </li>
-//         );
-//     }
-// }
-const LocationItem = ({marker, name}) =>{
-    return (
-        <li role="button"
-            className="place"
-            tabIndex="0"
-            onKeyPress={marker}
-            onClick={marker}>
-            <p
-                className='place-link'>
-                {name}
-            </p>
-        </li>
-    );
+export default class LocationItem extends React.Component {
+    render() {
+        return (
+            <li role="button"
+                className="place"
+                tabIndex="0"
+                onKeyPress={this.props.openInfoWindow.bind(this, this.props.data.marker)}
+                onClick={this.props.openInfoWindow.bind(this, this.props.data.marker)}>
+                <p
+                    className='place-link'>
+                    {this.props.data.name}
+                </p>
+            </li>
+        );
+    }
 }
-
-export default LocationItem;    
